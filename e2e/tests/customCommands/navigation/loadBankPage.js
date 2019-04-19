bfba4070ff975465b951f0fb836a6b536d7304ef
Amar <alubana@teksystems.com>
2019-04-18 21:58:21 -0400

    set image back circlci with and remove yarn

--- e2e/tests/customCommands/navigation/loadBankPage.js
+++ e2e/tests/customCommands/navigation/loadBankPage.js
@@ -9,7 +9,7 @@ exports.command = function (pageData = {}) {
   const _self = this;
   let {page, acceptAlert, pageLocator} = pageData;
 
-  page = _.isEmpty(page) ? '/' : _.trim(page);
+  page = _.isEmpty(page) ? '' : _.trim(page);
   pageLocator = _.isEmpty(pageLocator) ? '#root' : _.trim(pageLocator);
   acceptAlert = _.isEmpty(acceptAlert) ? false : _.trim(acceptAlert);
 
